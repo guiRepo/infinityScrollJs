e848b7d6a8b269ff7e2e14ccb04a0a7327e86e80
Guilherme Andre de Melo <guilhermeanm95@hotmail.com>
2021-01-19 10:02:49 -0300

    Filter of posts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # infinityScrollJs
+# Algoritimo separado por blocos lógicos
+# Uso de Arrow Function e Clousures
+# Algoritimo feito com JavaScript puro
--- app.js
+++ app.js
@@ -1,54 +1,82 @@
-const postsContainer = document.querySelector('#posts-container')
-const loaderContainer = document.querySelector('.loader')
-let page = 1
+const postsContainer = document.querySelector("#posts-container");
+const loaderContainer = document.querySelector(".loader");
+const filterInput = document.querySelector("#filter");
+
+let page = 1;
 
 const getPosts = async () => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`)
-    return response.json()
-   
-}
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`
+  );
+  return response.json();
+};
+
+const generatePostsTemplate = posts => posts.map (({ id, title, body }) => `
+        <div class="post">
+            <div class="number">${id}</div>
+            <div class="post-info">
+                <h2 class="post-title">${title}</h2>
+                <p class="post-body">${body}</p>
+            </div>
+        </div> 
+  `).join("");
 
 const addPostIntoDOM = async () => {
-    const posts = await getPosts()
-    const postsTemplate = posts.map(({ id, title, body})=> `
-    <div class="post">
-        <div class="number">${id}</div>
-        <div class="post-info">
-            <h2 class="post-title>${title}</h2>
-            <p class="post-body">${body}</p>
-        </div>
-    </div>
-    `).join('')
-    
-    postsContainer.innerHTML += postsTemplate
-}
+  const posts = await getPosts();
+  const postsTemplate = generatePostsTemplate(posts)
+  postsContainer.innerHTML += postsTemplate;
+};
 
-addPostIntoDOM()
-
-
-const getNextPosts= () => {
-    page++
-    addPostIntoDOM()
-}
+const getNextPosts = () => {
+  setTimeout(() => {
+    page++;
+    addPostIntoDOM();
+  }, 300);
+};
 
 const removeLoader = () => {
-    setTimeout(() => {
-        loaderContainer.classList.remove('show')
-        getNextPosts()
-    }, 1000);
-}
+  setTimeout(() => {
+    loaderContainer.classList.remove("show");
+    getNextPosts();
+  }, 1000);
+};
 
-const showLoader= () => {
-    loaderContainer.classList.add('show')
-    removeLoader()
-}
+const showLoader = () => {
+  loaderContainer.classList.add("show");
+  removeLoader();
+};
 
-
-window.addEventListener('scroll', () => {
-    const { clientHeight, scrollHeight, scrollTop } = document.documentElement
-    const isPageBottomAlmostReached = scrollTop+ clientHeight >= scrollHeight - 10
-
-    if (isPageBottomAlmostReached){
-        showLoader()
+const handleScrollToPageBottom = () => {
+    const { clientHeight, scrollHeight, scrollTop } = document.documentElement;
+    const isPageBottomAlmostReached =
+      scrollTop + clientHeight >= scrollHeight - 10;
+  
+    if (isPageBottomAlmostReached) {
+      showLoader();
     }
-})
+  }
+
+const showPostIfMatchInputValue = inputValue => post => { //clousure function
+    const postTitle = post.querySelector(".post-title").textContent.toLowerCase();
+    const postBody = post.querySelector(".post-body").textContent.toLowerCase();
+    const postContainsInputValue = postTitle.includes(inputValue) || postBody.includes(inputValue)
+
+    if (postContainsInputValue) {
+      post.style.display = "flex";
+      return;
+    }
+
+    post.style.display = "none";
+  }
+
+const handleInputValue = event => {
+    const inputValue = event.target.value.toLowerCase();
+    const posts = document.querySelectorAll(".post");
+  
+    posts.forEach(showPostIfMatchInputValue(inputValue));
+  }
+
+addPostIntoDOM();
+
+window.addEventListener("scroll", handleScrollToPageBottom)
+filterInput.addEventListener("input", handleInputValue);
